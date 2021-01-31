b38edad2d01a76b18a2cb4aa64148692f1c69992
Saruar Hosen <77750320+saruarhosenn@users.noreply.github.com>
2021-01-31 23:51:07 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+	// POPUP VIDEO
+	$('.popup-video').magnificPopup({
+		type: 'iframe',
+		iframe: {
+			markup: '<div class="mfp-iframe-scaler">' +
+				'<div class="mfp-close"></div>' +
+				'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+				'</div>',
+			patterns: {
+				youtube: {
+					index: 'youtube.com/',
+					id: 'v=',
+					src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+				},
+				vimeo: {
+					index: 'vimeo.com/',
+					id: '/',
+					src: '//player.vimeo.com/video/%id%?autoplay=1'
+				},
+				gmaps: {
+					index: '//maps.google.',
+					src: '%id%&output=embed'
+				}
+
+			},
+
+			srcAction: 'iframe_src',
+		}
+	});
+
+});
